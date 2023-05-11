afb8d2f29370ab7b2c760151099dea450727d020
Alonso Villegas <alonsovillegas816@gmail.com>
2023-05-11 12:04:19 -0500

    remove autopairs

--- nvim/after/plugin/autopairs.lua
+++
@@ -1,9 +0,0 @@
-local Rule = require("nvim-autopairs.rule")
-local npairs = require("nvim-autopairs")
-local cond = require("nvim-autopairs.conds")
-
-npairs.setup({})
-
-npairs.add_rules({
-	Rule("<", ">", { "typescript" }):with_pair(cond.not_before_regex("[%s<]")):with_cr(cond.none()),
-})
