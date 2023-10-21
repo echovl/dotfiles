687e7b44b44d52366bb33761cb7592afa2cd1e7f
Alonso Villegas <alonsovillegas816@gmail.com>
2023-10-20 20:54:56 -0500

    update nvim

--- global/nvim/lua/plugins/color.lua
+++ global/nvim/lua/plugins/color.lua
@@ -3,6 +3,6 @@ return {
 	-- { "rose-pine/neovim",         name = "rose-pine", lazy = true },
 	-- { "ellisonleao/gruvbox.nvim", lazy = true },
 	{ "sainnhe/gruvbox-material", lazy = true },
-	-- { "catppuccin/nvim", name = "catppuccin", lazy = true, opts = { flavour = "mocha" } },
+	{ "catppuccin/nvim", name = "catppuccin", lazy = true, opts = { flavour = "mocha" } },
 	-- { "navarasu/onedark.nvim",    lazy = true }
 }
